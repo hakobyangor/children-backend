0bd63914e86fe6de7cd97c00043f37cb7653cd46
Gor Hakobyan <gor@simplytechnologies.net>
2023-04-07 10:28:17 +0400

    check

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolvers } from './user.resolvers';
 import { PrismaService } from 'src/prisma.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [UserResolvers, UserService, PrismaService],
 })
 export class UserModule {}
